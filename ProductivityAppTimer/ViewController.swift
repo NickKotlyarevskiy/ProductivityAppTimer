7c7911218c4497f6cf001ad5d2a8fed45cec51ed
Nikita Kotlyarevskiy <nikitakotlyarevskiy@MacBook-Air--Nikita.local>
2024-03-20 14:44:07 +0300

    Debugged a lot of issues

--- ProductivityAppTimer/ViewController.swift
+++ ProductivityAppTimer/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var mainTimer = Timer()
+    
     @IBOutlet weak var timer: UILabel!
     @IBOutlet weak var timerProgress: UIProgressView!
     
@@ -21,41 +23,42 @@ class ViewController: UIViewController {
     }
 
     func resetTimer() {
-        minutes = 45
+        minutes = 1
         seconds = 0
         timerProgress.progress = 0
     }
 
     func updateUI() {
-        var minutesString = String(format: "%02d", minutes)
-        var secondsString = String(format: "%02d", seconds)
+        let minutesString = String(format: "%02d", minutes)
+        let secondsString = String(format: "%02d", seconds)
         timer.text = "\(minutesString):\(secondsString)"
     }
     
     @IBAction func startButton(_ sender: UIButton) {
+        mainTimer.invalidate()
         resetTimer()
         updateUI()
         
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] timer in
-            if seconds.self == 0 {
-                minutes -= 1
-                seconds = 59
-                updateUI()
+        mainTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.minutes == 0 && self.seconds == 0 {
+                self.mainTimer.invalidate()
+            } else if self.seconds.self == 0 {
+                self.minutes -= 1
+                self.seconds = 59
+                self.updateUI()
             } else {
-                seconds -= 1
-                updateUI()
+                self.seconds -= 1
+                self.updateUI()
             }
         }
-        var progressCount = Float(minutes)
-        timerProgress.progress = 0.0 - progressCount
     }
     
     @IBAction func resetButton(_ sender: UIButton) {
+        mainTimer.invalidate()
         resetTimer()
         updateUI()
     }
     
 }
 
-// DEBUG: Многократное нажатие на клопку старт разгоняет таймер
 // TODO: реализовать функционал timerProgress'а
